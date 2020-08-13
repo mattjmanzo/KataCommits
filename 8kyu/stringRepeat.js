4e0ab9e3bea319b010cb86e8f5375b55075a2d37
Matthew Manzo <matthew.manzo1@gmail.com>
2020-08-13 11:00:10 -0400

    solution 2

--- 8kyu/stringRepeat.js
+++ 8kyu/stringRepeat.js
@@ -1,3 +1,13 @@
+// String Repeat
+
+// Write a function called repeatString which repeats the given string src exactly count times.
+
+// Solution 1
+
 function repeatStr(n, s) {
   return s.repeat(n);
 }
+
+// Solution 2
+
+repeatStr = (n, s) => s.repeat(n);
