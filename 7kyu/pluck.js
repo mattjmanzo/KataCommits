df225dbd368128a1d1db7bd54c51ba13d90fcd9f
Matthew Manzo <matthew.manzo1@gmail.com>
2020-09-02 17:04:37 -0400

    solution 1

---
+++ 7kyu/pluck.js
@@ -0,0 +1,16 @@
+// Pluck
+
+// Implement a function which takes a sequence of objects and a property name, and returns a sequence containing the named property of each object.
+
+// For Example:
+
+// pluck([{a:1}, {a:2}], 'a') -> [1,2]
+// pluck([{a:1, b:3}, {a:2}], 'b') -> [3, undefined]
+
+// Solution 1
+
+function pluck(objs, name) {
+  return objs.map(function (o) {
+    return o[name];
+  });
+}
