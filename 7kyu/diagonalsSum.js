5671c7735b267cb67eb29b01c0e401b060e64bb8
Matthew Manzo <matthew.manzo1@gmail.com>
2020-09-08 14:38:42 -0400

    solution 2

--- 7kyu/diagonalsSum.js
+++ 7kyu/diagonalsSum.js
@@ -9,3 +9,17 @@ function sum(matrix) {
 
   return matrix.reduce((s, ar, i) => s + ar[i] + ar[n - i - 1], 0);
 }
+
+// Solution 2
+
+function sum(matrix) {
+  if (!matrix.length) return 0;
+  for (
+    var i = 0, j = 0, k = matrix[i].length - 1, sum = 0;
+    i < matrix.length;
+    i++
+  ) {
+    sum += matrix[i][j++] + matrix[i][k--];
+  }
+  return sum;
+}
