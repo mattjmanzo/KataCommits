23bb939f146e4e590cb1b88dfdbaa06829535cf0
Matthew Manzo <matthew.manzo1@gmail.com>
2020-09-15 16:54:51 -0400

    solution 1

---
+++ 7kyu-2/test123.js
@@ -0,0 +1,7 @@
+// Testing 1-2-3
+
+// Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering. Write a function which takes a list of strings and returns each line prepended by the correct number. The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+// Solution 1
+
+let number = (a) => a.map((v, i) => `${i + 1}: ${v}`);
