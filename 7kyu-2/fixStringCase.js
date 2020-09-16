bd04519bafeda56ae618f48b3637c37a1dc43ed3
Matthew Manzo <matthew.manzo1@gmail.com>
2020-09-16 11:02:21 -0400

    solution 1

--- 7kyu-2/fixStringCase.js
+++ 7kyu-2/fixStringCase.js
@@ -2,9 +2,8 @@
 
 // In this Kata, you will be given a string that may have mixed uppercase and lowercase letters and your task is to convert that string to either lowercase only or uppercase only based on:
 
-// - make as few changes as possible.
-
-// - if the string contains equal number of uppercase and lowercase letters, convert the string to lowercase.
+// make as few changes as possible.
+// if the string contains equal number of uppercase and lowercase letters, convert the string to lowercase.
 
 // Solution 1
 
