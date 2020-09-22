faf159664f520375918a586f60f9e2055e333676
Matthew Manzo <matthew.manzo1@gmail.com>
2020-09-22 08:53:49 -0400

    solution 2

--- 7kyu-2/coloredTriangles.js
+++ 7kyu-2/coloredTriangles.js
@@ -16,3 +16,38 @@ const triangle = (row) => {
   }
   return reduced.length > 1 ? triangle(reduced) : reduced;
 };
+
+// Solution 2
+
+function triangle(row) {
+  return row.length == 1 ? row : triangle(processedRow(row));
+}
+
+function processedRow(row) {
+  return row
+    .split("")
+    .map((el, i) =>
+      i == row.length - 1 ? null : sumLetters(row[i], row[i + 1])
+    )
+    .join("");
+}
+
+function sumLetters(a, b) {
+  switch (a + b) {
+    case "BG":
+    case "GB":
+    case "RR":
+      return "R";
+      break;
+    case "BR":
+    case "RB":
+    case "GG":
+      return "G";
+      break;
+    case "RG":
+    case "GR":
+    case "BB":
+      return "B";
+      break;
+  }
+}
