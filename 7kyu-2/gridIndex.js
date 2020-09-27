bdd4723b8e28537526b212a585fc02bb4dddcf76
Matthew Manzo <matthew.manzo1@gmail.com>
2020-09-27 19:51:11 -0400

    solution 2

--- 7kyu-2/gridIndex.js
+++ 7kyu-2/gridIndex.js
@@ -6,3 +6,8 @@ const gridIndex = (grid, indices) => {
   const flatList = grid.flat(2);
   return indices.map((indice) => flatList[indice - 1]).join("");
 };
+
+// Solution 2
+
+const gridIndex = (grid, indices) =>
+  indices.map((c) => grid.flat(2)[c - 1]).join("");
