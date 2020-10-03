b6540c77983c37a943846e7032f249ebc5703fb7
Matthew Manzo <matthew.manzo1@gmail.com>
2020-10-03 16:19:04 -0400

    solution 2

--- 7kyu-2/highestLowest.js
+++ 7kyu-2/highestLowest.js
@@ -8,3 +8,10 @@ function highAndLow(numbers) {
   numbers = numbers.split(" ");
   return `${Math.max(...numbers)} ${Math.min(...numbers)}`;
 }
+
+// Solution 2
+
+function highAndLow(numbers) {
+  var arr = numbers.split(" ");
+  return Math.max.apply(null, arr) + " " + Math.min.apply(null, arr);
+}
