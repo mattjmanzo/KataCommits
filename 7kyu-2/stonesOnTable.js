d63fcd69a0fa0b630523629a67d7b91ad94989a6
Matthew Manzo <matthew.manzo1@gmail.com>
2020-10-05 14:33:13 -0400

    solution 1

---
+++ 7kyu-2/stonesOnTable.js
@@ -0,0 +1,15 @@
+// Stones on the Table
+
+// There are some stones on Bob's table in a row, and each of them can be red, green or blue, indicated by the characters R, G, and B. Help Bob find the minimum number of stones he needs to remove from the table so that the stones in each pair of adjacent stones have different colours.
+
+// Solution 1
+
+function solve(stones) {
+  let counter = 0;
+  for (let i = 1; i < stones.length; i++) {
+    if (stones[i] === stones[i - 1]) {
+      counter++;
+    }
+  }
+  return counter;
+}
