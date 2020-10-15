e4af2d81b3634b8238e1a589f477d9e24c4907b0
Matthew Manzo <matthew.manzo1@gmail.com>
2020-10-15 14:29:52 -0400

    solution 1

---
+++ 7kyu-2/leastLarger.js
@@ -0,0 +1,15 @@
+// Least Larger 
+
+// Given an array of numbers and an index, return the index of the least number larger than the element at the given index, or -1 if there is no such index ( or, where applicable, Nothing or a similarly empty value ).
+
+// Solution 1 
+
+function leastLarger(a,i) {
+    let diff, index=-1;
+    for (let n = 0; n < a.length; n++)
+      if ((a[n]-a[i] > 0) && (diff === undefined || a[n]-a[i]<diff)) {
+        diff = a[n]-a[i];
+        index=n;
+      }
+    return index;
+  }
