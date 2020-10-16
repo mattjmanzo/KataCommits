245fd1264d357aa5c808712003f8c41fa7b497b0
Matthew Manzo <matthew.manzo1@gmail.com>
2020-10-16 16:47:54 -0400

    solution 2

--- 7kyu-2/leastLarger.js
+++ 7kyu-2/leastLarger.js
@@ -13,3 +13,13 @@ function leastLarger(a,i) {
       }
     return index;
   }
+
+  // Solution 2 
+
+  function leastLarger(a, i) {
+    const targetVal = a[i]
+    const largerVals = a.filter(num => num > targetVal)
+    const leastLargeVal = Math.min(...largerVals)
+    
+    return a.findIndex(num => num === leastLargeVal)
+}
