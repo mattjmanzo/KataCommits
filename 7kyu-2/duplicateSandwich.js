340f232e2ec2ec74aae02a62b9dc14b42a3acdfd
Matthew Manzo <matthew.manzo1@gmail.com>
2020-10-23 14:46:19 -0400

    solution 2

--- 7kyu-2/duplicateSandwich.js
+++ 7kyu-2/duplicateSandwich.js
@@ -23,3 +23,10 @@ function duplicateSandwich(arr) {
       }
     }
   }
+
+  // Solution 2 
+
+  const duplicateSandwich = (list) => {
+    let duplicated = [...list].find(a => list.indexOf(a) !== list.lastIndexOf(a))
+    return list.slice(list.indexOf(duplicated) + 1, list.lastIndexOf(duplicated))
+  }
