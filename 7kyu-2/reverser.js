770e8296bdbdddafbe907d3a389a55e52df22082
Matthew Manzo <matthew.manzo1@gmail.com>
2020-10-28 18:06:36 -0400

    solution 1

---
+++ 7kyu-2/reverser.js
@@ -0,0 +1,14 @@
+// Reverser 
+
+// Impliment the reverse function, which takes in input n and reverses it. For instance, reverse(123) should return 321. You should do this without converting the inputted number into a string.
+
+// Solution 1 
+
+function reverse(n){
+    let rev = 0;
+    while (n) {
+        rev = rev * 10 + n % 10;
+        n = Math.floor(n/10);
+    }
+    return rev;
+}
