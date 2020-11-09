d975db4e82f202dd6cd4fd21e10458b2819f30ac
Matthew Manzo <matthew.manzo1@gmail.com>
2020-11-08 20:37:11 -0500

    solution 1

---
+++ 7kyu-2/stringEndsWith.js
@@ -0,0 +1,9 @@
+// String Ends With?
+
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+// Solution 1 
+
+function solution(str, ending){
+    return str.endsWith(ending);
+  }
