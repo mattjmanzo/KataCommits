d7d69d5694d25d27cd63b84fec1e295892e1bcc2
Matthew Manzo <matthew.manzo1@gmail.com>
2020-11-14 16:08:30 -0500

    solution 1

---
+++ 7kyu-3/stringDoubles.js
@@ -0,0 +1,16 @@
+// String Doubles 
+
+// In this Kata, you will write a function doubles that will remove double string characters that are adjacent to each other.
+
+// Solution 1 
+
+function doubles(s) {
+    const cs = [];
+    for (const c of s) {
+      if (cs.length != 0 && cs[cs.length - 1] == c)
+        cs.pop();
+      else
+        cs.push(c);
+    }
+    return cs.join('');
+  }
