fd3bd972be8c1282e044774ded7a8305ac4805f6
Matthew Manzo <matthew.manzo1@gmail.com>
2020-11-18 15:11:19 -0500

    solution 2

--- 7kyu-3/formTheMinimum.js
+++ 7kyu-3/formTheMinimum.js
@@ -8,3 +8,13 @@ function minValue(values){
     let arr = Array.from(new Set(values))
     return parseInt(arr.sort().join(''))
   }
+
+// Solution 2 
+
+function minValue(values){
+  const n = values
+    .filter((e, i, arr) => arr.indexOf(e) === i)
+    .sort()
+    .join('');
+  return Number(n);
+}
