19a84c3f0d95d0b607cb13ba5ac2392b4184bb78
Matthew Manzo <matthew.manzo1@gmail.com>
2020-12-02 16:30:41 -0500

    solution 1

---
+++ 7kyu-3/validSpacing.js
@@ -0,0 +1,7 @@
+// Valid Spacing 
+
+// Your task is to write a function called valid_spacing() or validSpacing() which checks if a string has valid spacing. The function should return either True or False. For this kata, the definition of valid spacing is one space between words, and no leading or trailing spaces. Below are some examples of what the function should return.
+
+// Solution 1 
+
+const validSpacing=s=>s.replace(/\s+/g," ").trim()==s;
