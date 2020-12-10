b95e9b9dbecc236949c831291d52f89eeb731bbe
Matthew Manzo <matthew.manzo1@gmail.com>
2020-12-10 13:42:32 -0500

    solution 2

--- 7kyu-3/sharedBit.js
+++ 7kyu-3/sharedBit.js
@@ -5,3 +5,9 @@
 function sharedBits(a, b) {
     return (a & b).toString(2).replace(/0/g, '').length > 1
     }
+
+// Solution 2 
+
+function sharedBits(a, b) {
+    return a & b & (a & b) - 1 ? true : false;  
+  }
