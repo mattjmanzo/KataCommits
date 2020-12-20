4860691efae050876548901c8559dbf644cf3025
Matthew Manzo <matthew.manzo1@gmail.com>
2020-12-19 20:03:35 -0500

    solution 1

---
+++ 7kyu-3/boiledEggs.js
@@ -0,0 +1,9 @@
+// Boiled Eggs 
+
+// Implement a function, which takes a non-negative integer, representing the number of eggs to boil. It must return the time in minutes (integer), which it takes to have all the eggs boiled.
+
+// Solution 1 
+
+function cookingTime(eggs) {
+    return 5 * Math.ceil(eggs / 8);
+  }
