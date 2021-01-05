fdad65910cf4fe3b5ed954252556e38bb10adbf8
Matthew Manzo <matthew.manzo1@gmail.com>
2021-01-05 17:49:32 -0500

    solution 1

---
+++ 7kyu-3/sprayingTrees.js
@@ -0,0 +1,11 @@
+// Spraying Trees
+
+// 
+
+// Solution 1 
+
+let task = (w,n,c) => {
+    let obj = {'Monday':'James','Tuesday':'John','Wednesday':'Robert','Thursday':'Michael','Friday':'William'};
+    return `It is ${w} today, ${obj[w]}, you have to work, you must spray ${n} trees and you need ${n*c} dollars to buy liquid`;
+  };
+  
