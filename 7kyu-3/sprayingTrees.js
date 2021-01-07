9fbb0fc01417933c2ca66dfa3306f6ff01390157
Matthew Manzo <matthew.manzo1@gmail.com>
2021-01-07 17:34:19 -0500

    solution 2

--- 7kyu-3/sprayingTrees.js
+++ 7kyu-3/sprayingTrees.js
@@ -8,4 +8,16 @@ let task = (w,n,c) => {
     let obj = {'Monday':'James','Tuesday':'John','Wednesday':'Robert','Thursday':'Michael','Friday':'William'};
     return `It is ${w} today, ${obj[w]}, you have to work, you must spray ${n} trees and you need ${n*c} dollars to buy liquid`;
   };
-  
+
+// Solution 2 
+
+const task = function(w, n, c) {
+  const dic = {
+    'Monday'   : 'James',
+    'Tuesday'  : 'John',
+    'Wednesday': 'Robert',
+    'Thursday' : 'Michael',
+    'Friday'   : 'William'
+  }
+  return `It is ${w} today, ${dic[w]}, you have to work, you must spray ${n} trees and you need ${n * c} dollars to buy liquid`
+}
