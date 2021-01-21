147ddf4d7d58f887fdbc6d5414c87df879e32c0d
Matthew Manzo <matthew.manzo1@gmail.com>
2021-01-21 15:42:46 -0500

    solution 1

---
+++ 7kyu-3/simpleLetterRemoval.js
@@ -0,0 +1,16 @@
+// Simple Letter Removal 
+
+// Solution 1 
+
+function solve(s, k){
+    const abc = "abcdefghijklmnopqrstuvwxyz"
+    
+    for (let i=0; i<abc.length; i+=1) {
+      while(s.includes(abc[i]) && k > 0) {
+        s = s.replace(abc[i], "")
+        k -= 1
+      }
+    }
+    
+    return s
+  }
