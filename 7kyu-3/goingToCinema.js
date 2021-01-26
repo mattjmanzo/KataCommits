cf59d44a42fe83782dd2a8637a82b3eb6bc2585c
Matthew Manzo <matthew.manzo1@gmail.com>
2021-01-25 20:09:36 -0500

    solution 2

--- 7kyu-3/goingToCinema.js
+++ 7kyu-3/goingToCinema.js
@@ -13,3 +13,19 @@ function movie(card, ticket, perc) {
     }
     return n;
 };
+
+// Solution 2 
+
+function movie(card, ticket, perc) {
+    let a = 0,
+        b = card,
+        trips = 0;
+    
+    while (Math.ceil(a) <= Math.ceil(b)) {
+      ++trips;
+      b += ticket * Math.pow(perc, trips);
+      a += ticket;
+    }
+    
+    return trips;
+  };
