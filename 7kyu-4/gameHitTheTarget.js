9f51f74a163ec008c44445e4c34b40f3566d95ff
Matthew Manzo <matthew.manzo1@gmail.com>
2021-02-02 17:46:48 -0500

    solution 2

--- 7kyu-4/gameHitTheTarget.js
+++ 7kyu-4/gameHitTheTarget.js
@@ -6,3 +6,7 @@ const solution = mtrx =>{
     const arr = mtrx.filter(el => el.includes('>') && el.includes('x') )[0] || []
     return arr.indexOf('>') < arr.indexOf('x') 
   }
+
+// Solution 2 
+
+solution=_=>_.some(__=>/.*x.*<|.*>.*x/.test(__))
