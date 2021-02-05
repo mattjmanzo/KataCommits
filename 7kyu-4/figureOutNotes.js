a06b92cfc612aead4be11b6ceb3e01e16627e241
Matthew Manzo <matthew.manzo1@gmail.com>
2021-02-05 16:09:48 -0500

    solution 2

--- 7kyu-4/figureOutNotes.js
+++ 7kyu-4/figureOutNotes.js
@@ -6,3 +6,10 @@ const whatNote = (string, fret) => {
     let array = ['C', 'C#', 'D','D#','E','F','F#', 'G', 'G#', 'A','A#', 'B']
     return array[(fret + array.indexOf(string.toUpperCase())) % 12]  
   };
+
+// Solution 2 
+
+function whatNote (str, fret) {
+  let dic = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#'];
+  return dic[(dic.indexOf(str.toUpperCase()) + fret) % 12]
+};
