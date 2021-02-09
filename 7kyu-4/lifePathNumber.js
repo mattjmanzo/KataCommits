a450a4e2f7078405a07b1ab70e9d4ed52f36c70b
Matthew Manzo <matthew.manzo1@gmail.com>
2021-02-08 20:15:41 -0500

    solution 2

--- 7kyu-4/lifePathNumber.js
+++ 7kyu-4/lifePathNumber.js
@@ -5,3 +5,15 @@
 function lifePathNumber(dateOfBirth) {
     return dateOfBirth.replace(/\D/g,'') % 9 || 9;
   }
+
+// Solution 2 
+
+function lifePathNumber(dateOfBirth) {
+  var num = dateOfBirth.split("-").join("");
+  
+  do{
+    num = (""+num).split("").reduce((p,c) => +p+ +c);
+  }while(num > 9);
+  
+  return num;
+}
