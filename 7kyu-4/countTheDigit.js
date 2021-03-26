86f8f70cb762a00dac211f8ba16cbaeb91ba81a5
Matthew Manzo <matthew.manzo1@gmail.com>
2021-03-26 10:34:31 -0400

    solution 2

--- 7kyu-4/countTheDigit.js
+++ 7kyu-4/countTheDigit.js
@@ -9,3 +9,13 @@ function nbDig(n, d) {
           square.forEach((s)=>s==d?res++:null)
         }return res;
     }
+
+// Solution 2 
+
+function nbDig(n, d) {
+    var o = '';
+      for(var i = 0; i <= n; i++){
+        o += Math.pow(i, 2);
+      }
+    return o.split(d).length-1
+  }
